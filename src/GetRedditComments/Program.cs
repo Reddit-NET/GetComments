7990f30609d02fec0eb07aaacb74995f77ccb42a
Kris Craig <kriscraig@php.net>
2021-02-14 11:31:59 -0800

    Created solution.

---
+++ src/GetRedditComments/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GetRedditComments
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: GetRedditComments <Reddit App ID> <Reddit Refresh Token> [Reddit Access Token]");
+            }
+            else
+            {
+                Workflow workflow = new Workflow(args);
+                workflow.Run();
+            }
+        }
+    }
+}
